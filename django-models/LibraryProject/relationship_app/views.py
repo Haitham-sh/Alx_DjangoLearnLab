50dbfdb5495e3cf8224f299d4fc373c281574627
Haitham-sh <haithamelsherbiny81@gmail.com>
2025-09-08 00:36:15 +0300

    project Implement Role-Based Access Control in Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth import login
+from django.contrib.auth.decorators import user_passes_test
 
 # Create your views here.
 
@@ -26,3 +27,25 @@ class Register(CreateView):
     form_class = UserCreationForm()
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
+
+def is_admin(user):
+     return user.is_authenticated and hasattr(user, 'UserProfile') and user.UserProfile.role == 'Admin'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
+
+def is_librarian(user):
+     return user.is_authenticated and hasattr(user, 'UserProfile') and user.UserProfile.role == 'Librarian'
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html', {"message": "Welcome, Librarian!"})
+
+def is_member(user):
+     return user.is_authenticated and hasattr(user, 'UserProfile') and user.UserProfile.role == 'Member'
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html', {"message": "Welcome, Member!"})
+
