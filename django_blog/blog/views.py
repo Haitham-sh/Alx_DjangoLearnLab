9adeeaed0e640e9ac33a287a152064c99079af65
Haitham-sh <haithamelsherbiny81@gmail.com>
2025-10-03 02:06:17 +0300

    redirected the user when he edited his profile to the profile page

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -9,7 +9,6 @@ from .models import Profile, Post
 from .forms import UserEditForm, ProfileEditForm
 from django.urls import reverse
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -55,8 +54,6 @@ def edit_profile(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-            # redirect and show success message
-            messages.success(request, 'Profile updated successfully')
             return redirect('profile')
     return render(request, 'blog/edit_profile.html', {
         'user_form': user_form,
