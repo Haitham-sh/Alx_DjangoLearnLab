ac13cdf8885690b8eb51e26b783982181dc2ae5e
Haitham-sh <haithamelsherbiny81@gmail.com>
2025-10-08 01:24:35 +0300

    project Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -8,6 +8,7 @@ from django.contrib.auth import get_user_model
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     password2 = serializers.CharField(write_only=True)
+    username = serializers.CharField()
     
     class Meta:
         model = User
