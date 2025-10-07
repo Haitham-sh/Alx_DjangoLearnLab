2a3a9197c75fdb50bcf19123b53113b3c525fe2a
Haitham-sh <haithamelsherbiny81@gmail.com>
2025-10-07 17:56:16 +0300

    project Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,25 +1,33 @@
 from rest_framework import serializers
 from .models import User
 from rest_framework.authtoken.models import Token
-from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
 
 
-class registerSerializer(serializers.ModelSerializer):
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True)
+    
     class Meta:
         model = User
-        password = serializers.CharField(write_only=True)
-        fields = '__all__'
-        
-        
-        def create(self, validated_data):
-            user = get_user_model().objects.create_user(**validated_data)
-            Token.objects.create(user=user)
-            return user
+        fields = ['id', 'username', 'email', 'password', 'password2', 'first_name', 'last_name']
+    
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Passwords must match."})
+        return attrs
+    
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
 
 class loginSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'password']
+        
 
 
 class ProfileSerializer(serializers.ModelSerializer):
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.response import Response
 from .models import User
-from .serializers import UserSerializer, loginSerializer
+from .serializers import RegisterSerializer, loginSerializer, ProfileSerializer
 from django.urls import reverse_lazy
 from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
 from rest_framework import status
 from django.contrib.auth import authenticate
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 
@@ -16,7 +18,7 @@ from django.contrib.auth import authenticate
 # Create your views here.
 class RegistrationView(generics.CreateAPIView):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = RegisterSerializer
     success_url = reverse_lazy('login')
 
 class LoginView(APIView):
@@ -36,9 +38,12 @@ class LoginView(APIView):
               return Response({'error': 'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUEST)
 
 class ProfileView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
         user = request.user
-        serializer = UserSerializer(user)
+        serializer = ProfileSerializer
         return Response(serializer.data)
     
 
